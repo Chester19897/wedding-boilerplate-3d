5358cc04bf33ece8c724ee13d59bfaff05e92333
chester <chester@techainer.com>
2023-11-14 16:39:29 +0700

    fix content

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,84 +2,182 @@ import React from 'react'
 
 const NavMenu = () => {
   return (
-    <nav className="hidden md:block sticky top-0 z-[100] backdrop-blur-lg bg-white/70 shadow">
-      <section className="md:container md:mx-auto px-4">
-        <section className="flex flex-wrap mx-4 px-4">
-          <section className="grid grid-cols-12 w-full">
-            <section className="col-span-3 flex items-center">
-              <h1>
-                <a className="logo flex items-center justify-center gap-1 text-5xl text-[#837a7a]">
-                  Việt
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 30.22 30.01"
-                    width="11"
-                    height="11"
-                    className="ml-0.5"
-                  >
-                    <defs></defs>
-                    <g id="a"></g>
-                    <g id="b">
-                      <g id="c">
-                        <path
-                          className="d"
-                          d="M13.69,30.01c-.66-4.72-4.16-8.45-7.36-11.99S-.1,10.37,0,5.61c.02-1.06,.22-2.14,.75-3.06C2.1,.19,5.39-.55,7.93,.41c2.54,.97,4.37,3.26,5.51,5.72,1.33-2.73,3.78-4.97,6.72-5.72,2.94-.75,6.33,.15,8.27,2.48,2.38,2.86,2.2,7.2,.54,10.52s-4.52,5.87-7.33,8.3c-2.81,2.43-5.71,4.92-7.47,8.19"
-                        ></path>
+    <>
+      <nav className="hidden md:block sticky top-0 z-[100] backdrop-blur-lg bg-white/70 shadow">
+        <section className="md:container md:mx-auto px-4">
+          <section className="flex flex-wrap mx-4 px-4">
+            <section className="grid grid-cols-12 w-full">
+              <section className="col-span-3 flex items-center">
+                <h1>
+                  <a className="logo flex items-center justify-center gap-1 text-5xl text-[#837a7a]">
+                    Việt
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 30.22 30.01"
+                      width="11"
+                      height="11"
+                      className="ml-0.5 fill-[#837a7a]"
+                    >
+                      <defs></defs>
+                      <g id="a"></g>
+                      <g id="b">
+                        <g id="c">
+                          <path
+                            className="d"
+                            d="M13.69,30.01c-.66-4.72-4.16-8.45-7.36-11.99S-.1,10.37,0,5.61c.02-1.06,.22-2.14,.75-3.06C2.1,.19,5.39-.55,7.93,.41c2.54,.97,4.37,3.26,5.51,5.72,1.33-2.73,3.78-4.97,6.72-5.72,2.94-.75,6.33,.15,8.27,2.48,2.38,2.86,2.2,7.2,.54,10.52s-4.52,5.87-7.33,8.3c-2.81,2.43-5.71,4.92-7.47,8.19"
+                          ></path>
+                        </g>
                       </g>
-                    </g>
-                  </svg>
-                  Ngọc
-                </a>
-              </h1>
-            </section>
-            <section className="col-span-9">
-              <ul className="flex justify-end">
-                <li className="px-5">
-                  <a
-                    href="#section0"
-                    className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
-                  >
-                    Home
+                    </svg>
+                    Ngọc
                   </a>
-                </li>
-                <li className="px-5">
-                  <a
-                    href="#section1"
-                    className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
-                  >
-                    Couple
-                  </a>
-                </li>
-                <li className="px-5">
-                  <a
-                    href="#section2"
-                    className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
-                  >
-                    Story
-                  </a>
-                </li>
-                <li className="px-5">
-                  <a
-                    href="#section3"
-                    className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
-                  >
-                    Khoảnh khăc
-                  </a>
-                </li>
-                <li className="px-5">
-                  <a
-                    href="#section5"
-                    className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
-                  >
-                    Gửi lời chúc
-                  </a>
-                </li>
-              </ul>
+                </h1>
+              </section>
+              <section className="col-span-9">
+                <ul className="flex justify-end">
+                  <li className="px-5">
+                    <a
+                      href="#section0"
+                      className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li className="px-5">
+                    <a
+                      href="#section1"
+                      className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
+                    >
+                      Couple
+                    </a>
+                  </li>
+                  <li className="px-5">
+                    <a
+                      href="#section2"
+                      className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
+                    >
+                      Story
+                    </a>
+                  </li>
+                  <li className="px-5">
+                    <a
+                      href="#section3"
+                      className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
+                    >
+                      Khoảnh khăc
+                    </a>
+                  </li>
+                  <li className="px-5">
+                    <a
+                      href="#section5"
+                      className="block cursor-pointer text-[16px] font-[500] px-2.5 py-8 text-[#3f3f46]"
+                    >
+                      Gửi lời chúc
+                    </a>
+                  </li>
+                </ul>
+              </section>
             </section>
           </section>
         </section>
-      </section>
-    </nav>
+      </nav>
+      <div className="md:hidden navbar bg-base-100 z-[100] px-5 py-5 sticky top-0 backdrop-blur-lg bg-white/70 shadow flex">
+        <div className="inline-flex items-center flex-1">
+          <h1>
+            <a
+              className="flex items-center justify-center gap-1 text-5xl text-[#837a7a]"
+              href="/"
+            >
+              Việt{' '}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 30.22 30.01"
+                width="11"
+                height="11"
+                className="ml-0.5 fill-[#837a7a]"
+              >
+                <defs></defs>
+                <g id="a"></g>
+                <g id="b">
+                  <g id="c">
+                    <path
+                      className="d"
+                      d="M13.69,30.01c-.66-4.72-4.16-8.45-7.36-11.99S-.1,10.37,0,5.61c.02-1.06,.22-2.14,.75-3.06C2.1,.19,5.39-.55,7.93,.41c2.54,.97,4.37,3.26,5.51,5.72,1.33-2.73,3.78-4.97,6.72-5.72,2.94-.75,6.33,.15,8.27,2.48,2.38,2.86,2.2,7.2,.54,10.52s-4.52,5.87-7.33,8.3c-2.81,2.43-5.71,4.92-7.47,8.19"
+                    ></path>
+                  </g>
+                </g>
+              </svg>{' '}
+              Ngọc
+            </a>
+          </h1>
+        </div>
+        <div className="inline-flex items-center flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                ></path>
+              </svg>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#fff] rounded-box w-52"
+            >
+              <li className="flex py-1">
+                <a
+                  href="#section0"
+                  className="text-[16px] font-[500] text-[#3f3f46]"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="flex py-1">
+                <a
+                  href="#section1"
+                  className="text-[16px] font-[500] text-[#3f3f46]"
+                >
+                  Couple
+                </a>
+              </li>
+              <li className="flex py-1">
+                <a
+                  href="#section2"
+                  className="text-[16px] font-[500] text-[#3f3f46]"
+                >
+                  Story
+                </a>
+              </li>
+              <li className="flex py-1">
+                <a
+                  href="#section3"
+                  className="text-[16px] font-[500] text-[#3f3f46]"
+                >
+                  Khoảnh khăc
+                </a>
+              </li>
+              <li className="flex py-1">
+                <a
+                  href="#section5"
+                  className="text-[16px] font-[500] text-[#3f3f46]"
+                >
+                  Gửi lời chúc
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 

0cbfef032295c07884efb1c308ac3866398648b2
MeanNguyen <mean@techainer.com>
2023-11-14 19:31:15 +0700

    add slide

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js',
-		'./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-		],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('tailwindcss-animated'),
-		require('flowbite/plugin')
-	],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('tailwindcss-animated'),
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin.cjs')
+  ]
 }
